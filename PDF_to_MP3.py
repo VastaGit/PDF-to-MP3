dc2d5853e493c52f16f8621c0785b7728d10ade4
Vasta_git <askatseitakunov@gmail.com>
2023-04-25 17:00:58 +0300

    pulling test

--- PDF_to_MP3.py
+++ PDF_to_MP3.py
@@ -8,7 +8,7 @@ with open(f'{file_name}.pdf', 'rb') as f:
     text = high_level.extract_text(f) #extracting text from pdf file
 
 #this was added in features branch
-
+#also added for pulling
 speech = gTTS(text = text, lang = language, slow = False) #setting speech parameters
 print("Processing...")
 speech.save(f"{file_name}.mp3")
