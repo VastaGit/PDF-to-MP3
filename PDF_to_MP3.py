1f83fbfca6aa7390095f46cd25abe0e40600b9f6
Vasta_git <askatseitakunov@gmail.com>
2023-04-25 16:56:07 +0300

    adding comments

--- PDF_to_MP3.py
+++ PDF_to_MP3.py
@@ -7,6 +7,8 @@ file_name = "sample" #declaring your file name
 with open(f'{file_name}.pdf', 'rb') as f:
     text = high_level.extract_text(f) #extracting text from pdf file
 
+#this was added in features branch
+
 speech = gTTS(text = text, lang = language, slow = False) #setting speech parameters
 print("Processing...")
 speech.save(f"{file_name}.mp3")
