3860241edc8c06463a1a3fd37c39ac4e568d014e
Vasta_git <askatseitakunov@gmail.com>
2023-04-25 14:22:08 +0300

    second commit

--- PDF_to_MP3.py
+++ PDF_to_MP3.py
@@ -5,8 +5,7 @@ from pdfminer import high_level
 language = "en" #setting language settings
 file_name = "sample" #declaring your file name
 with open(f'{file_name}.pdf', 'rb') as f:
-    text = high_level.extract_text(f)
-
+    text = high_level.extract_text(f) #extracting text from pdf file
 
 speech = gTTS(text = text, lang = language, slow = False)
 print("Processing...")
